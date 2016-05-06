70d22b4b43db473b958a5682962c68a475f5f220
Horacio Conde <horacio.conde@hotmail.com>
2016-05-06 11:41:00 -0500

    Added files via upload
    Auto adjustable exact-minute clock.

---
+++ self_adjust_clock.cc
@@ -0,0 +1,51 @@
+#include <sys/time.h>
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <iomanip>
+
+/*
+ * This programs should be compiled in c++0x mode (C++11)
+ * g++ -std=c++0x self_adjust_clock.cc
+ */
+
+int main(void) {
+  // 1. Get full current time
+  timeval curTime;
+  gettimeofday(&curTime, NULL);
+  int milli = curTime.tv_usec / 1000;
+  tm *the_time = localtime(&curTime.tv_sec);
+
+  // 2. Find out the number of milliseconds until the next complete second
+  int sleep_ms = 1000 - milli;
+
+  // 3. Sleep for the number of milliseconds until the next complete second
+  std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
+
+  // 4. Again, get full current time
+  gettimeofday(&curTime, NULL);
+  milli = curTime.tv_usec / 1000;
+  the_time = localtime(&curTime.tv_sec);
+
+  std::cout << std::setfill('0') << std::setw(2) << the_time -> tm_hour;
+  std::cout << ":" << std::setfill('0') << std::setw(2) << the_time -> tm_min;
+  std::cout << ":" << std::setfill('0') << std::setw(2) << the_time -> tm_sec << std::endl;
+
+  // 5. Find out the number of milliseconds until the next complete minute
+  int sec = the_time->tm_sec;
+  int sleep_s = 60000 - (1000 * sec);
+
+  // 6. Sleep for the number of milliseconds until the next complete minute
+  std::this_thread::sleep_for(std::chrono::milliseconds(sleep_s));
+
+  // At this point milliseconds and seconds should be adjusted to zero
+  gettimeofday(&curTime, NULL);
+  milli = curTime.tv_usec / 1000;
+  the_time = localtime(&curTime.tv_sec);
+
+  std::cout << std::setfill('0') << std::setw(2) << the_time -> tm_hour;
+  std::cout << ":" << std::setfill('0') << std::setw(2) << the_time -> tm_min;
+  std::cout << ":" << std::setfill('0') << std::setw(2) << the_time -> tm_sec << std::endl;
+
+  return 0;
+}
